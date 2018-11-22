3fceafc36c04c1dc8a98ed62776ecf9370c226ef
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-22 15:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 // Code your solution in this file
 function findMatching (list, name) {
-  return list.filter(function (driver) {
-    return driver.toLowerCase() === name.toLowerCase();
-  });
+  return list.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase();} );
 }
 
 function fuzzyMatch (list, includes) {
   let length = includes.length;
-  return list.filter(function (driver) {
-    return driver.slice(0, length) === includes;
-  });
+  return list.filter(function (driver) { return driver.slice(0, length) === includes;} );
 }
 
 function matchName (list, name) {
