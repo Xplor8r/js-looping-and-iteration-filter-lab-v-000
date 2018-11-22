269f3935fdc0d4f5d11769b8e5e266a856081a21
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-22 15:49:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(name) {
+  for (const name of name) {
+    if (drivers.name === name) {
+      console.log(drivers.name);
+    }
+  }
+}
+
+function fuzzyMatch() {
+
+}
+
+function matchName() {
+
+}
